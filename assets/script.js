3e5fde7f402a4dced2653c7de4357f8c9318e9f3
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-10-09 13:04:49 -0600

    functioning timer

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,35 @@
-//on start quiz button click, start timer and hide welcome page 
-var startbtn = document.getElementById("#start");
-btn.addEventlistener("click", startQuiz);
-function startQuiz () {
-    //move to questions 
+// get variables from HTML
+var quizQuestions = document.getElementById('questions');
+var choices = document.getElementById('options');
 
-    //start timer
-  var timerStart = function startTimer {
-        var countDown = 60; 
-        setInterval(function()){
-            if (countDown >= 0){
-                
-            }
-        }
-    },
-    console.log("I have started my quiz");
-};
-// Once timer is started display quiz questions, these need to be able to be clicked and answers saved 
-// Declare variable for quiz questions 
+
+//on start quiz button click, start timer and hide welcome page 
+var startButton = document.querySelector('button');
+var countDownEl = document.getElementById('seconds');
+
+function updateCountdown () {
+ var timeLeft = 60;
+
+ var timeInterval = setInterval(function() {
+     if (timeLeft > 1) {
+    countDownEl.textContent = timeLeft + ' seconds remaining';
+    timeLeft--;
+     } else if (timeLeft === 1) {
+         countDownEl.textContent = timeLeft + ' second remaining';
+         timeLeft--;
+    } else {
+        countDownEl.textContent = '';
+        clearInterval(timeInterval);
+        displayMessage();
+    }
+ }, 1000);
+}
+
+
+
+startButton.addEventListener("click", (updateCountdown));
+
+//create an array for questions 
 
 var codeQuestions = [
     {
@@ -40,9 +53,71 @@ var codeQuestions = [
     }
 ];
 
+//function to display code questions 
+
+function quiz () {
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function countDown() {
+    timeLeft--;
+
+    document.getElementById("#seconds").innerHTML = ( "timeleft" );
+    if (timeleft > 0) {
+        setTimeout(countDown, 1000);
+    }
+};
+
+setTimeout(countDown, 6000);
+
+var seconds = 60;
+var secondsCount = document.getElementById("seconds").textContent;
+var countDown = setInterval(function(){
+    secondsCount--;
+    document.getElementById("seconds").textContent = seconds;
+    if (seconds <= 0) clearInterval(countDown);
+    }, 60000);
+
+/*var startTimer = setInterval(function(){
+    if (timerStart <= 0) {
+        clearInterval(timerStart);
+    }
+    document.getElementById("#timerCount").value = 60 - timerStart;
+    timerStart -= 1;
+}, 60000);
+
+
+//btn.addEventlistener("click", startTimer);
+
+*/
+// Once timer is started display quiz questions, these need to be able to be clicked and answers saved 
+// Declare variable for quiz questions 
+
 
 
 // fucntion to generate quiz
-
-
 
