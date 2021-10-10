fedd102a7d31e212e86bc04c0c1823db41a9b1d1
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-10-10 08:03:19 -0600

    displaying code questions and keeping score

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 // get variables from HTML
 var quizQuestions = document.getElementById('questions');
 var choices = document.getElementById('options');
-
+var title = document.getElementById('quiz-start');
+var score = 0;
 
 //on start quiz button click, start timer and hide welcome page 
 var startButton = document.querySelector('button');
@@ -23,6 +24,10 @@ function updateCountdown () {
         displayMessage();
     }
  }, 1000);
+
+ title.classList.add('hidden');
+ quizQuestions.classList.remove('hidden');
+ updateQuestion();
 }
 
 
@@ -45,75 +50,86 @@ var codeQuestions = [
     {
         question: "Which HTML element holds the JavaScript?",
         answers: {
-            a: '<js>',
-            b: '<Script>',
-            c: '<javascript>',
-            d: '<script',
+            a: 'js',
+            b: 'Script',
+            c: 'javascript',
+            d: 'script',
         },
+        correct: 'd'
+    },
+    {
+        question: "who am I",
+        answers: {
+            a: 'dog',
+            b: 'cat',
+            c: 'perdson',
+            d: 'computer',
+        },
+        correct: 'c'
+
     }
 ];
 
 //function to display code questions 
+var questionIndex = 0;     
+var singleQuestion = document.getElementById('question');
+var choices = document.querySelectorAll('.answer-choice');
 
-function quiz () {
-    
+function updateQuestion () {
 
+singleQuestion.innerHTML = codeQuestions[0].question;
+choices[0].innerHTML = codeQuestions[0].answers.a
+choices[1].innerHTML = codeQuestions[0].answers.b
+choices[2].innerHTML = codeQuestions[0].answers.c
+choices[3].innerHTML = codeQuestions[0].answers.d
+
+for (i=0; i<4; i++) {
+    choices[i].addEventListener("click", changeQuestion);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function countDown() {
-    timeLeft--;
-
-    document.getElementById("#seconds").innerHTML = ( "timeleft" );
-    if (timeleft > 0) {
-        setTimeout(countDown, 1000);
-    }
 };
 
-setTimeout(countDown, 6000);
+function changeQuestion (event) {
 
-var seconds = 60;
-var secondsCount = document.getElementById("seconds").textContent;
-var countDown = setInterval(function(){
-    secondsCount--;
-    document.getElementById("seconds").textContent = seconds;
-    if (seconds <= 0) clearInterval(countDown);
-    }, 60000);
-
-/*var startTimer = setInterval(function(){
-    if (timerStart <= 0) {
-        clearInterval(timerStart);
+    if (event.target.id == codeQuestions[questionIndex].correct) {
+        score++
+        console.log(score);
     }
-    document.getElementById("#timerCount").value = 60 - timerStart;
-    timerStart -= 1;
-}, 60000);
+   
+    questionIndex++;
+    if (questionIndex == codeQuestions.length) {
+        alert("End of quiz:" + score)
+        return;
+        //display an element hide ending page element.classlist.remove. class hidden 
+    }
+
+    console.log("1",questionIndex);
+console.log(codeQuestions[1].answers.a);
+console.log(codeQuestions[questionIndex].answers.a);
+console.log(choices);
+singleQuestion.innerHTML = codeQuestions[questionIndex].question;
+choices[0].innerHTML = codeQuestions[questionIndex].answers.a;
+choices[1].innerHTML = codeQuestions[questionIndex].answers.b;
+choices[2].innerHTML = codeQuestions[questionIndex].answers.c;
+choices[3].innerHTML = codeQuestions[questionIndex].answers.d;
+
+};
 
 
-//btn.addEventlistener("click", startTimer);
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Once timer is started display quiz questions, these need to be able to be clicked and answers saved 
 // Declare variable for quiz questions 
 
